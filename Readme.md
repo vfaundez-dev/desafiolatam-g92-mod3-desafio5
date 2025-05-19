df5cf63ce6b4ae4d1e0f4e37e701b0aa18f19774
Vladimir Faundez Hernandez <v.faundezh@gmail.com>
2025-05-19 09:13:30 -0400

    Corregido estilos y orden en los metodos JS. Agregado Readme.md

---
+++ Readme.md
@@ -0,0 +1,19 @@
+## DESAFIO LATAM - _Curso Fullstack Javascript G92_
+### Modulo 3 - Javascript
+
+**_Desarrollado por Vladimir Faundez_**
+> Nota: `Esto realizado con propósitos académicos y de enseñanza. Favor no realizar copias directas del material aqui subido.`
+
+### * Desafio 5 - Todo List
+
+Desarrollado con `HTML`, `CSS` y `Javascript`, consiste en el desarrollo de un sencillo `Todo List`, el cual agrega, elimina y marca **completada** tareas:
+
+
+#### Logros ✅  
+
+- [x] Agregar tareas (Todos).
+- [x] Borrar una tarea ingresada.
+- [x] Contar total de tareas ingresadas.
+- [x] Marcar una tarea completada.
+- [x] Contar total de tareas realizadas.
+- [x] Inicializar array de tareas con al menos 3 elementos.
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,52 +3,33 @@ const inputTodo = document.getElementById('inputTodo');
 const btnAdd = document.querySelector('.btn-add');
 const tableTodo = document.getElementById('tableTodo');
 const tbody = tableTodo.querySelector('tbody');
-const todoList = [];
-let lastTodoId = 0;
-
-// Agregar TODO a la lista
-btnAdd.addEventListener('click', event => {
-    const todo = inputTodo.value.trim();
-
-    // Validar que el input no esté vacío
-    if ( todo.length === 0 ) {
-        alert('Debes ingresar una tarea!');
-        return;
-    }
-
-    // Generar objeto
-    const todoObj = {
-        id: ++lastTodoId,
-        dataId: todo.length,
-        name: todo,
+// Listado de TODOS iniciales
+const todoList = [
+    {
+        id: 1,
+        dataId: 1,
+        name: 'Revisar que el codigo compile',
         completed: false
-    } 
+    },
+    {
+        id: 2,
+        dataId: 2,
+        name: 'Terminar los desafios',
+        completed: false
+    },
+    {
+        id: 3,
+        dataId: 3,
+        name: 'Corregir los bugs',
+        completed: false
+    }
+];
 
-    // Agregar tarea a array
-    todoList.push(todoObj);
-    // Pintar en HTML
-    tbody.innerHTML += generateTemplateTodo(todoObj);
-    // Limpiar input
-    inputTodo.value = '';
-    // Actualizar contadores
-    updateDetails();
+// Asignar ID's correlativos en orden
+let lastTodoId = 3;
 
-});
 
-// Listener para eliminar y marcar como completada
-tbody.addEventListener('click', (event) => {
-    const trTodo = event.target.closest('tr');
-    const btnRemove = event.target.closest('.btn-remove');
-    const checkboxCompleted = event.target.closest('.todo-completed');
-
-    // Comprobar boton eliminar
-    if (btnRemove) return deleteTodo(trTodo);
-
-    // Comprobar checkbox
-    if ( checkboxCompleted )
-        return completedTodo(trTodo, checkboxCompleted.checked);
-
-});
+/*--- Generadores ---*/
 
 // Plantilla para todos
 const generateTemplateTodo = (todo) => {
@@ -70,11 +51,41 @@ const generateTemplateTodo = (todo) => {
     `;
 }
 
+// Generar todos los Todos
 const generateAllTodos = () => {
     tbody.innerHTML = '';
     todoList.forEach( todo => tbody.innerHTML += generateTemplateTodo(todo) );
 }
 
+// Ejecutar primer render
+generateAllTodos();
+
+
+/*--- Metodos Principales ---*/
+
+// Metodo Agregar
+const addTodo = (todo) => {
+
+    // Generar objeto
+    const todoObj = {
+        id: ++lastTodoId,
+        dataId: todo.length,
+        name: todo,
+        completed: false
+    }
+
+    // Agregar tarea a array
+    todoList.push(todoObj);
+    // Pintar en HTML
+    tbody.innerHTML += generateTemplateTodo(todoObj);
+    // Limpiar input
+    inputTodo.value = '';
+    // Actualizar contadores
+    updateDetails();
+
+}
+
+// Metodo Eliminar
 const deleteTodo = (tr) => {
     const todoID = parseInt(tr.dataset.id);
     // Busca el indice del todo si existe segun todoID
@@ -87,10 +98,12 @@ const deleteTodo = (tr) => {
     }
 }
 
+// Metodo Completar
 const completedTodo = (tr, checkCompleted) => {
     const todoID = parseInt(tr.dataset.id);
     // Busca el indice del todo si existe segun todoID
     const todoIndex = todoList.findIndex( todo => todo.id === todoID );
+    // Si existe el todo, lo actualiza
     if (todoIndex !== -1) { // -1 indica que no encontro el indice
         const tdNameTodo = tr.querySelector('td:nth-child(2)');
         todoList[todoIndex].completed = checkCompleted;
@@ -99,6 +112,7 @@ const completedTodo = (tr, checkCompleted) => {
     }
 }
 
+// Actualizar contadores
 const updateDetails = () => {
     const totalTodosSpan = document.getElementById('totalTodos');
     const completedTodosSpan = document.getElementById('completedTodos');
@@ -106,3 +120,35 @@ const updateDetails = () => {
     totalTodosSpan.innerHTML = todoList.length;
     completedTodosSpan.innerHTML = todoList.filter( todo => todo.completed ).length;
 }
+
+
+/*--- Listeners ---*/
+
+// Agregar TODO a la lista
+btnAdd.addEventListener('click', event => {
+    const todo = inputTodo.value.trim();
+
+    // Validar que el input no esté vacío
+    if ( todo.length === 0 ) {
+        alert('Debes ingresar una tarea!');
+        return;
+    }
+
+    return addTodo(todo);
+
+});
+
+// Listener para eliminar y marcar como completada
+tbody.addEventListener('click', (event) => {
+    const trTodo = event.target.closest('tr');
+    const btnRemove = event.target.closest('.btn-remove');
+    const checkboxCompleted = event.target.closest('.todo-completed');
+
+    // Comprobar boton eliminar
+    if (btnRemove) return deleteTodo(trTodo);
+
+    // Comprobar checkbox
+    if ( checkboxCompleted )
+        return completedTodo(trTodo, checkboxCompleted.checked);
+
+});
